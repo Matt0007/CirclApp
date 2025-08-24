9d16c492cbe7210b505af36445597a4b826f78b2
Matthias F <matthias.favrin@gmail.com>
2025-08-24 15:51:11 +0200

    image change to logo

--- app.config.js
+++ app.config.js
@@ -4,11 +4,11 @@ export default {
   slug: "circl-app",
   version: "1.0.0",
   orientation: "portrait",
-  icon: "./assets/icon.png",
+  icon: "./assets/images/icon.png",
   userInterfaceStyle: "automatic",
   scheme: "circlapp",
   splash: {
-    image: "./assets/splash-icon.png",
+    image: "./assets/images/splash-icon.png",
     resizeMode: "contain",
     backgroundColor: "#D97706"
   },
